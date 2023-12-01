61a31090895740dbc36591f33d29dae7fdbd7430
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2023-12-01 20:49:03 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+let attempts = 9;
+let roundNumber = 1;
+const guessedNumbers = [];
+
+function guessNumber() {
+    const guessInput = document.getElementById("guessInput");
+    const guessButton = document.querySelector("button");
+    const resultMessage = document.getElementById("result");
+    const roundNumberElement = document.getElementById("roundNumber");
+    const imageElement = document.getElementById("resultImage");
+
+    if (guessButton.disabled) {
+        resultMessage.textContent = "Игра окончена. Вы использовали все 10 попыток.";
+        return;
+    }
+
+    const userGuess = parseInt(guessInput.value);
+
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+        resultMessage.textContent = "Введите корректное число от 1 до 100.";
+        return;
+    }
+
+    if (guessedNumbers.includes(userGuess)) {
+        resultMessage.textContent = "Вы уже вводили это число. Попробуйте другое.";
+        return;
+    }
+
+    guessedNumbers.push(userGuess);
+    attempts--;
+
+    if (userGuess === secretNumber) {
+        resultMessage.textContent = `Поздравляем! Вы угадали число ${secretNumber}!`;
+        disableInputAndButton();
+        imageElement.src = "./assets/images/right.png";
+    } else {
+        const hint = userGuess < secretNumber ? "больше" : "меньше";
+        resultMessage.textContent = `Неверно. Загаданное число ${hint} вашего.`;
+        imageElement.src = "./assets/images/wrong.png";
+
+        if (attempts === 0) {
+            resultMessage.textContent = `К сожалению, вы проиграли. Загаданное число было: ${secretNumber}.`;
+            disableInputAndButton();
+            imageElement.src = "./assets/images/wrong.png";
+        }
+    }
+    roundNumber++;
+    roundNumberElement.textContent = roundNumber;
+    guessInput.value = "";
+    
+    if (roundNumber > 10) {
+        disableInputAndButton();
+    }
+}
+
+function disableInputAndButton() {
+    const guessInput = document.getElementById("guessInput");
+    const guessButton = document.querySelector("button");
+
+    guessInput.disabled = true;
+    guessButton.disabled = true;
+}
+
+document.getElementById("totalRounds").textContent = 10;
